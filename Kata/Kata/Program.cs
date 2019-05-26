56242cc93d7fcf546b904b8c5dc76b58e5eeb491
Rosana Castro <rcastrog91@gmail.com>
2019-05-26 00:38:28 -0400

    Adding interface

--- Kata/Kata/Program.cs
+++ Kata/Kata/Program.cs
@@ -10,6 +10,33 @@ namespace Kata
     {
         static void Main(string[] args)
         {
+            PricingRules pricingRules = new PricingRules();
+
+            Console.WriteLine("============>Stating execution <============");
+            Console.WriteLine("Pricing table");
+            Console.WriteLine("Item | Unit Price | Special Price");
+            foreach (var item in pricingRules.pricingList)
+            {
+                Console.WriteLine(item.Key + "    |" + item.Value.price + "          |" + item.Value.specialPriceCont + " for " +  item.Value.specialPrice);
+            }
+            CheckOut checkOut = new CheckOut(pricingRules);
+            Console.WriteLine();
+            Console.WriteLine("Scaning products");
+
+            Console.WriteLine("A");
+            checkOut.Scan("A");
+
+            Console.WriteLine("B");
+            checkOut.Scan("B");
+
+            Console.WriteLine("A");
+            checkOut.Scan("A");
+
+            Console.WriteLine("A");
+            checkOut.Scan("A");
+
+            Console.WriteLine("Total price : " + checkOut.Total());
+            Console.ReadLine();
         }
     }
 }

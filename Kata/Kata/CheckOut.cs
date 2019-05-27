ac727980cbf0be212506f4864a19d0b855b6192e
Rosana Castro <rcastrog91@gmail.com>
2019-05-26 20:46:17 -0400

    Adding ScanMultiple method. Modifying Total method.

--- Kata/Kata/CheckOut.cs
+++ Kata/Kata/CheckOut.cs
@@ -16,11 +16,19 @@ namespace Kata
             this.items = new Dictionary<string, int>();
             this.pricingRules = pricingRules;
         }
-        
+
         public void Scan(string item)
         {
             if (items.ContainsKey(item)) items[item]++;
             else items.Add(item, 1);
+        }
+
+        public void ScanMultiple(string items)
+        {
+            foreach (var item in items.Split(','))
+            {
+                Scan(item);
+            }
         }
 
         public int Total()
@@ -29,15 +37,23 @@ namespace Kata
 
             foreach (var item in items)
             {
-                Rule rule = pricingRules.pricingList[item.Key];
-                if (item.Value < rule.specialPriceCont)
-                    total += item.Value * rule.price;
-                else
+                if (pricingRules.pricingList.ContainsKey(item.Key))
                 {
-                    int quotient = item.Value / rule.specialPriceCont;
-                    int remainder = item.Value % rule.specialPriceCont;
-                    total += rule.specialPrice * quotient + remainder * rule.price;
+                    Rule rule = pricingRules.pricingList[item.Key];
+                    if (item.Value < rule.specialPriceCont)
+                        total += item.Value * rule.price;
+                    else
+                    {
+                        if (rule.specialPriceCont != 0)
+                        {
+                            int quotient = item.Value / rule.specialPriceCont;
+                            int remainder = item.Value % rule.specialPriceCont;
+                            total += rule.specialPrice * quotient + remainder * rule.price;
+                        }
+                        else total += item.Value * rule.price;
+                    }
                 }
+                else throw new ItemNotFoundException();
             }
             return total;
         }
